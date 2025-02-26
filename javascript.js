4fe9559ed923a816cb005c13c070b6df04a879fd
Gandalf2711 <190136093+Gandalf2711@users.noreply.github.com>
2025-02-26 22:06:41 +0100

    Add event on buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let nbOne;
 let nbTwo;
 let operator;
+let display = "";
+let array;
 
 function add (a, b) {
     return a + b;
@@ -17,6 +19,23 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 };
+
+function getOperator(disp) {
+    if(disp.includes("+")) {
+        operator = "+";
+    } else if (disp.includes("-")) {
+        operator = "-";
+    } else if (disp.includes("*")) {
+        operator = "*";
+    } else {
+        operator = "/";
+    };
+    return operator;
+};
+
+function split(arr) {
+    return array = display.split(operator);
+}
 
 function operate(a, b, op) {
     let result;
@@ -36,3 +55,30 @@ function operate(a, b, op) {
     }
     return result;
 };
+
+const btns = document.querySelectorAll("#btn");
+const para = document.querySelector("p");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        display = display + btn.innerHTML;
+        para.textContent = display;
+        return display;
+    });
+});
+
+const acBtn = document.querySelector(".btn-clear");
+
+acBtn.addEventListener("click", () => {
+    display = ""
+    para.textContent = display;
+    return display;
+});
+
+const equal = document.querySelector(".btn-equal");
+
+equal.addEventListener("click", () => {
+    getOperator(display);
+    split(display);
+    para.textContent = operate(parseInt(array[0]), parseInt(array[1]), operator);
+});
